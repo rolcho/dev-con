824ae8d21fdfab41d69732770634ba646d443d23
Roland Nagy <roland@cimem.hu>
2024-01-05 16:41:58 +0000

    Update eslint configuration and home page template

--- skeleton/src/app/home/home.page.ts
+++ skeleton/src/app/home/home.page.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonLabel,
+  IonList,
+  IonCard,
+  IonCardTitle,
+  IonCardHeader,
+  IonCardContent,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonLabel,
+    IonList,
+    IonCard,
+    IonCardTitle,
+    IonCardHeader,
+    IonCardContent,
+  ],
 })
 export class HomePage {
+  myArray: string[] = new Array(100).fill('mock text');
   constructor() {}
 }
