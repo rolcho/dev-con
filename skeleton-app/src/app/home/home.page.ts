fe0d42409a1fcfd6fded252bb1412d18d4534570
rolcho <roland@cimem.hu>
2024-01-05 19:37:23 +0000

    Add toggle functionality to filter even/odd items in array

--- skeleton-app/src/app/home/home.page.ts
+++ skeleton-app/src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
   IonHeader,
   IonToolbar,
@@ -10,6 +10,8 @@ import {
   IonCardTitle,
   IonCardHeader,
   IonCardContent,
+  IonToggle,
+  IonItem,
 } from '@ionic/angular/standalone';
 
 @Component({
@@ -28,9 +30,32 @@ import {
     IonCardTitle,
     IonCardHeader,
     IonCardContent,
+    IonToggle,
+    IonItem,
   ],
 })
-export class HomePage {
-  myArray: string[] = new Array(100).fill('mock text');
+export class HomePage implements OnInit {
+  myArray: string[] = [];
+  evenOdd: boolean = true;
+  filteredArray: string[] = [];
+
   constructor() {}
+
+  ngOnInit() {
+    for (let i = 0; i < 10; i++) {
+      this.myArray.push(`Item ${i}`);
+    }
+    this.updateArray();
+  }
+
+  toggleEvenOdd() {
+    this.evenOdd = !this.evenOdd;
+    this.updateArray();
+  }
+
+  updateArray() {
+    this.filteredArray = this.myArray.filter((_, i) =>
+      this.evenOdd ? i % 2 === 0 : i % 2 === 1
+    );
+  }
 }
